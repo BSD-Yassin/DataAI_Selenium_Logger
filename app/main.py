73c1c5909b167045e8923520d3307b90a5740dc4
BSD-Yassin <Yassin.bousaadi@gmail.com>
2022-11-01 21:11:50 +0100

    "Database WIP"

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from src import sel
 from src import back
-from src import database_up
+from src import database_loader
 import time
 import datetime
 import sys
 import os
 
 if __name__ == '__main__':
-    while True:
+    while True :
         instance = sel.Uc_instance()
         instance.session_uc()
         instance.xpath_logger()
-        print('---  Logged with --' + instance.user['username'] + '--at--' + f'{datetime.datetime.now()}  ---')
-        time.sleep(5)
+        
+        db_instance = database_loader.DB_connect()
+        db_instance.mySQL_login()
+        
+        print(instance.success)
+        
+        db_instance.write_db(instance.user['username'],
+                            instance.user['password'],
+                            instance.ip,
+                            failed=instance.success)
+
